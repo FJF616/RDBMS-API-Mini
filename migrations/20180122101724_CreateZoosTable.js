03167410f4a975c11ab7006dc8bfd8ddda1a1809
FJF616 <franzjfernando@hotmail.com>
2018-01-22 13:59:42 -0800

    post and get routes

--- migrations/20180122101724_CreateZoosTable.js
+++ migrations/20180122101724_CreateZoosTable.js
@@ -2,9 +2,20 @@
 exports.up = function(knex, Promise) {
   //this code executes when we run the migration
   //create the zoos table
+  return knex.schema.createTable('zoos', function(tbl) {
+      tbl.increments('id'); // by default it will be called id
+      
+      tbl
+      .string('name', 255)
+      .notNullable()
+      .unique('name', 'uq_zoos_name');
+      
+      tbl.timestamp('created_at').defaultTo(knex.fn.now());
+  })
 };
 
 exports.down = function(knex, Promise) {
   //this code runs when rolling back the migration
   //delete the zoos table
+  return knex.schema.dropTableIfExists('zoos');
 };
--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const knex = require('./database/db.js');
 const server = express();
 
 server.use(bodyParser.json());
 
 // endpoints here
+server.get('/', function(req, res) {
+    res.status(200).json({ success: true });
+});
+server.post('/api/zoos', function(req,res) {
+    const zoo = req.body;
 
+    knex
+    .insert(zoo)
+    .into('zoos')
+    .then(function(ids) {
+       res.status(201).json({ ids }); 
+    }).catch(function() {
+        res.status(500).json({ error: "failed to insert zoo"});
+    });
+});
+server.get('/api/zoos/:id', function(req,res) {
+    knex('zoos')
+        .where('id', id)
+        .then(function(records) {
+            res.status(201).json({ ids });
+        }).catch(function() {
+            res.status(500).json({ errorMessage: 'Could not get the Zoo' });
+        }); 
+    });
 const port = 3000;
 server.listen(port, function() {
     console.log(`Server Listening on ${port}`);
