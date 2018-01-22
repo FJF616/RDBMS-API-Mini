756fefe4bb97e86ff2eab1bb421aa104eff67cfb
FJF616 <franzjfernando@hotmail.com>
2018-01-22 10:21:52 -0800

    initial commit

---
+++ migrations/20180122101724_CreateZoosTable.js
@@ -0,0 +1,10 @@
+
+exports.up = function(knex, Promise) {
+  //this code executes when we run the migration
+  //create the zoos table
+};
+
+exports.down = function(knex, Promise) {
+  //this code runs when rolling back the migration
+  //delete the zoos table
+};
